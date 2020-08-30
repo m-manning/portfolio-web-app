0894fec78b015aef4ae829c1c85d10dbe8b8403a
Marcos <marcosmanning@outlook.com>
2020-08-30 23:50:42 +0100

    tests for adding and editing cv education

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,31 @@
 from django.test import TestCase
+from blog.models import CV
 
 class CVPageTest(TestCase):
 
     def test_uses_cv_template(self):
         response = self.client.get('/cv')
         self.assertTemplateUsed(response, 'cv/cv.html')
+
+    def test_cv_shows_all_items(self):
+        CV.objects.create(education='University of Birmingham')
+
+        response = self.client.get('/cv')
+
+        self.assertIn('University of Birmingham', response.content.decode())
+
+class CvModelTest(TestCase):
+
+    def test_adding_and_retrieving(self):
+        cv = CV()
+        cv.education = 'University of Birmingham'
+        cv.save()
+
+        saved_cv = CV.objects.all()
+        self.assertEqual(saved_cv.count(), 1)
+        
+        complete_cv = saved_cv[0]
+
+        self.assertEqual(complete_cv.education, 'University of Birmingham')
+
+
--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class CVTest(unittest.TestCase):  
@@ -9,17 +11,26 @@ class CVTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def test_can_view_cv(self):  
+    def site_login(self):
+        self.browser.get('http://localhost:8000/accounts/login')
+        self.browser.find_element_by_id('id_username').send_keys("YOUR_USERNAME")
+        self.browser.find_element_by_id('id_password').send_keys("YOUR_PASSWORD")
+        self.browser.find_element_by_id('submit').click()
 
-        #An employer checks out Marcos' CV
+    def test_admin_can_view_and_edit_cv(self): #needs admin permissions
+
+        self.site_login()
+        time.sleep(1)
+
+        #I take a look at my CV
         self.browser.get('http://localhost:8000/cv')
 
-        #They notice the header and page title have his name
+        #The header and page title have my name
         self.assertIn('Marcos Manning', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Marcos Manning', header_text)
 
-        #They notice his contact details 
+        #I can see my contact details
         contactTitle = self.browser.find_element_by_id('contact_details').text
         self.assertIn('Contact Details', contactTitle)
         #all the details are correct
@@ -28,14 +39,30 @@ class CVTest(unittest.TestCase):
         self.assertIn('07865155338', contact)
         self.assertIn('marcosmanning@outlook.com', contact)
 
-        #They take a look at his education history 
+        #I take a look at my education history 
+        educationTitle = self.browser.find_element_by_id('education_details').text
+        self.assertIn('Education', educationTitle)
+
+        #I edit the education history 
+        editCV = self.browser.find_element_by_id('edit_cv')
+        editCV.click()
+        time.sleep(1)
+        input_education = self.browser.find_element_by_id('id_education')
+        input_education.send_keys('University of Birmingham')
+        self.browser.find_element_by_id('submit').click()
+        time.sleep(1)
+
+        #verify all the updated details are correct
+        education = self.browser.find_element_by_class_name('education').text
+        self.assertIn('University of Birmingham', education)
+
         self.fail('Finish the test!')  
 
-        #They then take a look at his tech skills
+        #I take a look through my tech skills
 
-        #They then look through his work experience
+        #I take a look through my work experience
 
-        #Finally they can see his additional skills 
+        #Finally I check my addtional skills
 
 
 if __name__ == '__main__':  
